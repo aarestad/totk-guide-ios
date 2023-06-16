e3a29179069fcf4f352eac2e29f6c55fac2dee2c
Peter Aarestad <aarestad@gmail.com>
2023-06-16 17:36:03 -0500

    Initial Commit

---
+++ TOTKChecklist/TOTKChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOTKChecklistApp.swift
+//  TOTKChecklist
+//
+//  Created by Peter Aarestad on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TOTKChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
